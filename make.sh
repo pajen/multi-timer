49106bc119a155d5e210be173572476381582f74
Matthew Tole <matthewtole@gmail.com>
2013-11-11 19:07:27 +0000

    Add support for capturing screenshots of the app using RockShot.

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
-# jshint js/main.js || { exit 1; }
+jshint javascript/main.js || { exit 1; }
 jshint pebble/appinfo.json || { exit 1; }
-# uglifyjs js/main.js -o pebble/src/js/pebble-js-app.js
+uglifyjs javascript/libs/rockshot/src/firebase.js javascript/libs/rockshot/src/rockshot.js javascript/main.js -o pebble/src/js/pebble-js-app.js
 cd pebble
 pebble clean
 pebble build || { exit 1; }
-# rm src/js/pebble-js-app.js || { exit 1; }
+rm src/js/pebble-js-app.js || { exit 1; }
 if [ "$1" = "install" ]; then
     pebble install --logs
 fi
